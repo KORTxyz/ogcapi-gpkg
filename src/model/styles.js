8c2e210f4bd9884fd6e5419b1d4d19777a9e89de
Tino Kastbjerg Stigsen <tinoks@gmail.com>
2025-01-10 16:26:36 +0100

    Added sqlqueries to style model

--- src/model/styles.js
+++ src/model/styles.js
@@ -1,3 +1,20 @@
+const getStyles = async (db) => db.prepare(`
+  SELECT style, description
+  FROM  gpkgext_styles
+`).all(collectionId);
+
+const getStyle = (db, styleId) => db.prepare(`
+  SELECT style, description
+  FROM  gpkgext_styles
+  WHERE style=?
+`).get(styleId);
+
+const getStylesheet = (db, styleId, format) => db.prepare(`
+    SELECT stylesheet
+    FROM  gpkgext_stylesheet
+    WHERE styleId=? AND format=?
+`).pluck().get(styleId, format);
+
 
 const getCollectionStyles = async (db, collectionId) => db.prepare(`
     SELECT styleName,useAsDefault, description
@@ -17,8 +34,12 @@ const getCollectionStylesheet = (db, collectionId, styleId, format) => db.prepar
       WHERE f_table_name=? AND styleName=?
   `).pluck().get(collectionId, styleId);
 
-  export {
-    getCollectionStyles,
-    getCollectionStyle,
-    getCollectionStylesheet
-  }
+export {
+  getStyles,
+  getStyle,
+  getStylesheet,
+
+  getCollectionStyles,
+  getCollectionStyle,
+  getCollectionStylesheet
+}
